fc737b672408c70aff3751a44b06525d7395d1bd
sammyfe <oluwayanmife2@gmail.com>
2022-05-18 17:34:35 -0700

    frust

--- shell.c
+++ shell.c
@@ -1,40 +1,76 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * main - runs the shell program
+ * without_comment - deletes comments from the input
  *
- * Return: 0 on success
+ * @in: input string
+ * Return: input without comments
  */
-int main(void)
+char *without_comment(char *in)
 {
-    char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
-    char *PATH = NULL;
-    char **av;
-    int exitstatus = 0;
+	int i, up_to;
 
-    signal(SIGINT, SIG_IGN);
-    PATH = _getenv("PATH");
-    if (PATH == NULL)
-        return (-1);
-    while (1)
-    {
-        av = NULL;
-        prompt();
-        buffer = _read();
-        if (*buffer != '\0')
-        {
-            av = tokenize(buffer);
-            if (av == NULL)
-            {
-                free(buffer);
-                continue;
-            }
-            fullpathbuffer = _fullpathbuffer(av, PATH, copy);
-            if (checkbuiltins(av, buffer, exitstatus) == 1)
-                continue;
-            exitstatus = _forkprocess(av, buffer, fullpathbuffer);
-        }
-        else
-            free(buffer);
-    }
-    return (0);
+	up_to = 0;
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '#')
+		{
+			if (i == 0)
+			{
+				free(in);
+				return (NULL);
+			}
+
+			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+				up_to = i;
+		}
+	}
+
+	if (up_to != 0)
+	{
+		in = _realloc(in, i, up_to + 1);
+		in[up_to] = '\0';
+	}
+
+	return (in);
+}
+
+/**
+ * shell_loop - Loop of shell
+ * @datash: data relevant (av, input, args)
+ *
+ * Return: no return.
+ */
+void shell_loop(data_shell *datash)
+{
+	int loop, i_eof;
+	char *input;
+
+	loop = 1;
+	while (loop == 1)
+	{
+		write(STDIN_FILENO, "^-^ ", 4);
+		input = read_line(&i_eof);
+		if (i_eof != -1)
+		{
+			input = without_comment(input);
+			if (input == NULL)
+				continue;
+
+			if (check_syntax_error(datash, input) == 1)
+			{
+				datash->status = 2;
+				free(input);
+				continue;
+			}
+			input = rep_var(input, datash);
+			loop = split_commands(datash, input);
+			datash->counter += 1;
+			free(input);
+		}
+		else
+		{
+			loop = 0;
+			free(input);
+		}
+	}
 }
